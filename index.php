ac2c95f2b49871385de21715edd5246384839f65
Jonas Nunes <suporte@jonasnunes.com.br>
2026-01-27 21:20:28 -0300

    Iniciando CSS

--- index.php
+++ index.php
@@ -3,34 +3,39 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
   <title>Ordenador</title>
 </head>
 <body>
-  <header>
-    <h1>
-      User Management
-    </h1>
-  </header>
-  <div>
-    <form action="filtrar.php">
-      <select name="" id="">
-        <option value="selecionar">Selecionar</option>
-        <option value="nome">Nome (A - Z)</option>
-        <option value="email">Email (A - Z)</option>
-        <option value="idade">Por idade</option>
-        </select>
-    </form>
-  </div>
-  <main>
-    <i>User</i>
+  <div class="container">
+
+    <header>
+      <h1>
+        User Management
+      </h1>
+    </header>
     <div>
-      <h2>Nome</h2>
-      <p>Email@email.com</p>
+      <form action="filtrar.php">
+        <select name="" id="">
+          <option value="selecionar">Selecionar</option>
+          <option value="nome">Nome (A - Z)</option>
+          <option value="email">Email (A - Z)</option>
+          <option value="idade">Por idade</option>
+          </select>
+      </form>
     </div>
-    <p>Idade</p>
-  </main>
-  <footer>
-    <i>Cadastrar</i>
-  </footer>
+    <main>
+      <i>User</i>
+      <div>
+        <h2>Nome</h2>
+        <p>Email@email.com</p>
+      </div>
+      <p>Idade</p>
+    </main>
+    <footer>
+      <i>Cadastrar</i>
+    </footer>
+
+  </div>
 </body>
 </html>
