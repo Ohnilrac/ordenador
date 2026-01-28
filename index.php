a2bfd29e564727b908854550516441ec68ae78bc
Jonas Nunes <suporte@jonasnunes.com.br>
2026-01-27 21:10:20 -0300

    Criando estrutura HTML

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ordenador</title>
+</head>
+<body>
+  <header>
+    <h1>
+      User Management
+    </h1>
+  </header>
+  <div>
+    <form action="filtrar.php">
+      <select name="" id="">
+        <option value="selecionar">Selecionar</option>
+        <option value="nome">Nome (A - Z)</option>
+        <option value="email">Email (A - Z)</option>
+        <option value="idade">Por idade</option>
+        </select>
+    </form>
+  </div>
+  <main>
+    <i>User</i>
+    <div>
+      <h2>Nome</h2>
+      <p>Email@email.com</p>
+    </div>
+    <p>Idade</p>
+  </main>
+  <footer>
+    <i>Cadastrar</i>
+  </footer>
+</body>
+</html>
