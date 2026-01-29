7a0b61da56b3e7582e10d980cc354ae064ea7355
Jonas Nunes <suporte@jonasnunes.com.br>
2026-01-29 14:23:17 -0300

    Adicionando funcionalidade de filtro, retirando pasta de action não necessária e trocando área de idade por conta de usuário

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php 
 include_once("./config/db.php");
-
-
-$sql = "SELECT * FROM contas";
-$stmt = $pdo->prepare($sql);  
-$stmt->execute();
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +12,32 @@ $stmt->execute();
   <title>Ordenador</title>
 </head>
 <body>
+  <?php 
+  $sql = "SELECT * FROM contas";
+  $stmt = $pdo->prepare($sql);  
+  $stmt->execute();
+  
+  $colunas_permitidas = ['titular', 'email', 'conta'];
+
+  if(isset($_POST['filtragem']) && $_POST['filtragem'] != 'selecionar') {
+    $selecionado = $_POST['filtragem'];
+
+    if(in_array($selecionado, $colunas_permitidas)){
+      $sql = "SELECT * FROM contas ORDER BY {$selecionado}";
+    } else{
+      $sql = "SELECT * FROM contas";
+    }
+
+    $stmt = $pdo->prepare($sql);  
+    $stmt->execute();
+  } else{
+    $sql = "SELECT * FROM contas";
+    $stmt = $pdo->prepare($sql);  
+    $stmt->execute();
+  }
+  
+  ?>
+
   <div class="container">
 
     <header>
@@ -27,12 +48,12 @@ $stmt->execute();
     </header>
     <div class="filtro">
       <p>Filtrar</p>
-      <form action="filtrar.php" method="post">
-        <select name="" id="">
+      <form action="" method="post">
+        <select name="filtragem" id="filtragem" onchange="this.form.submit()">
           <option value="selecionar">Selecionar</option>
-          <option value="nome">Nome (A - Z)</option>
-          <option value="email">Email (A - Z)</option>
-          <option value="idade">Por idade</option>
+          <option value="titular" <?php echo ($selecionado == 'titular') ? 'selected="selected"' : 'selecionar'; ?>>Nome (A - Z)</option>
+          <option value="email" <?php echo ($selecionado == 'email') ? 'selected="selected"' : 'selecionar'; ?>>Email (A - Z)</option>
+          <option value="conta" <?php echo ($selecionado == 'conta') ? 'selected="selected"' : 'selecionar'; ?>>Por conta</option>
           </select>
       </form>
     </div>
@@ -48,7 +69,7 @@ $stmt->execute();
           <h2><?= htmlspecialchars($usuario['titular']) ?></h2> <!-- Comecei a utilizar o htmlspecialchars, para evitar problemas de injeção de html, por mais que tenhamos usado o prepare lá na solicitação dos dados do DB. Chamado de dados XSS, EX: <script>alert('escript de um engraçadinho')</script> -->
           <p><?= htmlspecialchars($usuario['email']) ?></p>
         </div>
-        <p class="idade">Idade</p>
+        <p class="conta"><?= htmlspecialchars($usuario['conta']) ?></p>
 
       </div>
         <hr><?php endforeach; ?> <!-- Aqui eu estou finalizando o fluxo que eu espero que o foreach faça -->
